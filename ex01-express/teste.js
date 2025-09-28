c75418ea8af7040fec23031f5fd9cb4cf1919744
Maria <110576299+md-sart@users.noreply.github.com>
2025-09-28 22:55:20 +0000

    .

--- ex01-express/teste.js
+++ ex01-express/teste.js
@@ -1,5 +1,6 @@
-    require('dotenv').config(); 
-    
-    console.log("--- Teste de Variável de Ambiente ---");
-    console.log("POSTGRES_URL: ", process.env.POSTGRES_URL);
-    console.log("-------------------------------------");
+// O CÓDIGO SELECIONADO ESTAVA AQUI:
+require('dotenv').config();
+
+console.log('--- Teste de Variável de Ambiente ---');
+console.log('POSTGRES_URL:', process.env.POSTGRES_URL);
+console.log('------------------------------------');
